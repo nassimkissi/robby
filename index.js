1658570688a5bbbd8029fbb2dfa2009082d859fc
Riunn <nassdev132@gmail.com>
2022-05-28 18:20:07 +0200

    First event handler

---
+++ index.js
@@ -0,0 +1,8 @@
+const { Client } = require('discord.js')
+const dotenv = require('dotenv')
+dotenv.config()
+const client = new Client({ intents: 1 })
+
+require('./utils/handlers/EventUtil')(client)
+
+client.login(process.env.DISCORD_TOKEN)
